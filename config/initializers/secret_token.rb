b87fc40f404d95bb27c34a636c45a574c6e3f965
Angel Pizarro <angel@upenn.edu>
2013-02-27 13:34:17 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collos::Application.config.secret_token = '7e38974ea707674bc9b2feb01b28032e51939ae792633d6f5192afd7688723beaee6339adb16525e33eeac95ef35ebbcdaa404409991d7676b30886e262917cf'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Collos::Application.routes.draw do
+  root :to => "home#index"
+end
