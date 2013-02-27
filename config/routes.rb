1e0a2d04179bf4190f83a920ab95164301dc4ea9
Angel Pizarro <angel@upenn.edu>
2013-02-27 16:31:12 -0500

    few small updates putting in skeleton of site. ready for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Collos::Application.routes.draw do
   root :to => "home#index"
+  match "/about", to: "home#about", as: 'about', format: "html"
+  match "/help", to: "home#help", as: 'help', format: "html"
+  match "/contact", to: "home#contact", as: 'contact', format: "html"
 end
