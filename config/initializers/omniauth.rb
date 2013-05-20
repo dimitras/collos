7b7d931f5836f5226b49851fcde32fd20c551ed5
Angel Pizarro <angel@upenn.edu>
2013-05-20 12:12:34 -0400

    small change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     unless Rails.env.production?
         provider :developer, fields: [:email,:name]
     end
+
     provider :identity, fields: [:email,:name], model: User, on_failed_registration:   UsersController.action(:register)
+
     provider :basecamp, ENV['BASECAMP_IDENTIFIER'], ENV['BASECAMP_SECRET']
+
 end
