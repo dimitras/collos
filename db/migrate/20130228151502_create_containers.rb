3f3ec421dfb16e3c8fb3ad393e0fb01de233b1be
Angel Pizarro <angel@upenn.edu>
2013-06-10 10:46:10 -0400

    made container.ancestry nullable

--- db/migrate/20130228151502_create_containers.rb
+++ db/migrate/20130228151502_create_containers.rb
@@ -3,7 +3,7 @@ class CreateContainers < ActiveRecord::Migration
     create_table :containers do |t|
       t.references :container_type
       t.string :name
-      t.string :ancestry, null: false, length: 500
+      t.string :ancestry, length: 500
       t.integer :ancestry_depth, default: 0
       t.integer :x, default: 0
       t.integer :y, default: 0
