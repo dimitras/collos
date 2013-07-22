b8ed42491f26bdf1b1c8a54afc6c354f4e5ff31f
Angel Pizarro <angel@upenn.edu>
2013-07-22 10:31:43 -0400

    added user <--> address habt model

---
+++ app/models/addresses_users.rb
@@ -0,0 +1,3 @@
+class AddressesUsers < ActiveRecord::Base
+  attr_accessible :address_id, :user_id
+end
---
+++ test/unit/addresses_users_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddressesUsersTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
