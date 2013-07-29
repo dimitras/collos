813d43564893371acecf5709a791acd5c17079b7
Angel Pizarro <delagoya@gmail.com>
2013-07-29 14:30:46 -0400

    added default column values for container_type cols

--- db/migrate/20130228151449_create_container_types.rb
+++ db/migrate/20130228151449_create_container_types.rb
@@ -3,11 +3,10 @@ class CreateContainerTypes < ActiveRecord::Migration
     create_table :container_types do |t|
       t.references :type
       t.string :name
-      t.integer :x_dimension
-      t.integer :y_dimension
-      t.string :x_coord_labels
-      t.string :y_coord_labels
-
+      t.integer :x_dimension, default: 1
+      t.integer :y_dimension, default: 1
+      t.string :x_coord_labels, default: 'number'
+      t.string :y_coord_labels, default: 'number'
       t.timestamps
     end
     add_index :container_types, :type_id
